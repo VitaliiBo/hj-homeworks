6e9c77ec39dc3874e039a47bd11ad9a9128fcc5f
VitaliiBo <boflying1@gmail.com>
2019-12-22 19:44:47 +0300

    Задача 1. Выпадающее меню(done)

--- html-element-collection/dropdown-menu/script.js
+++ html-element-collection/dropdown-menu/script.js
@@ -1,9 +1,6 @@
 const dropDown = document.querySelector('.wrapper-dropdown');
-
-
 dropDown.onclick = function () {
-  let classes = dropDown.className;
-  let classArray = classes.split(' ');
+  let classArray = dropDown.className.split(' ');
   if (classArray.includes('active')) {
     dropDown.className = classArray[0];
   } else {
